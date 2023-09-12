2603348f8cd5af759ddbe0c5c30a4dd96a7d2929
syifa dharmawan <dharmawansyifa@gmail.com>
2023-09-12 20:38:11 +0700

    updated README.md

--- main/tests.py
+++ main/tests.py
@@ -4,8 +4,8 @@ from main import views
 
 class AppTest(TestCase):
     def test_main_html_url_exists(self):
-        response = Client().get('/main/html/')
-        self.assertEqual(response.status_code, 404)
+        response = Client().get('/')
+        self.assertEqual(response.status_code, 200)
     def test_main_xml_url_exists(self):
         response = Client().get('/main/xml/')
         self.assertEqual(response.status_code, 404)
