48b59269e271ca9c3b5bde57dad6c5a8f6572552
syifa dharmawan <dharmawansyifa@gmail.com>
2023-09-12 20:17:07 +0700

    created test

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase, Client
+from main import views
+
+class AppTest(TestCase):
+    def test_main_html_url_exists(self):
+        response = Client().get('/main/html/')
+        self.assertEqual(response.status_code, 404)
+    def test_mainlist_xml_url_exists(self):
+        response = Client().get('/main/xml/')
+        self.assertEqual(response.status_code, 404)
+    def test_main_json_url_exists(self):
+        response = Client().get('/main/json/')
+        self.assertEqual(response.status_code, 404)
+    def test_view_html(self):
+        response = Client().get('/')
+        self.assertTemplateUsed(response, 'main.html')
